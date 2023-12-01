41c3d9562ac7878574adda3c1475567f216831ae
JhoamMolina <molina_504@hotmail.com>
2023-12-01 16:34:47 -0500

    tarea9

--- 01-MiPrimeraApp/src/screens/TareaScreen.tsx
+++ 01-MiPrimeraApp/src/screens/TareaScreen.tsx
@@ -21,6 +21,7 @@ const styles = StyleSheet.create({
   cajaMorada: {
     width: 100,
     height: 100,
+    top: 100,
     backgroundColor: '#5856D6',
     borderWidth: 10,
     borderColor: 'white',
